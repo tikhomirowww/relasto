1d2392d8f23eab5941cc749b661190fe309d50e4
Tikhomirowww <tihomirovi92@gmail.com>
2023-02-02 15:53:45 +0600

    [PM-862j57mpp]:hero layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import Hero from './components/Hero';
 
 const App = (): JSX.Element => (
   <>
-    <Hero />
     <h1>Property manager</h1>
   </>
 );
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './components/Hero/Hero.module.scss'
 
 import './styles/app.scss';
 import App from './App';

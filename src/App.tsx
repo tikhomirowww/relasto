acbf08ebc328fca2f1d2344ba984e67c21d8b9f4
Umidae <umiart55@gmail.com>
2023-02-03 19:10:16 +0600

    deleted useless code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React, { FC} from 'react';
-import NumberCount from './components/NumberCount';
 
 const App: FC = () => (
   <>
     <h1>Property manager</h1>
-    <NumberCount/>
   </>
 );
 
